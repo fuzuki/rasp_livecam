11c08b891ad74d40238724cea817f7102b3e9bbf
fuzuki <kfuzuki@gmail.com>
2018-07-09 08:02:00 +0000

    first commit

---
+++ setenv.sh
@@ -0,0 +1,16 @@
+#/bin/sh
+
+cd `dirname $0`
+
+sudo apt-get install ffmpeg
+
+sudo cp ./ffserver /etc/init.d/
+sudo cp ./ffserver.conf /etc/
+sudo cp ./ffmpeg /etc/init.d/
+
+sudo useradd webcam
+sudo adduser webcam video
+
+sudo update-rc.d ffserver defaults 90
+sudo update-rc.d ffmpeg defaults 92
+
